4a07720a512606df7cc9a8e6302ab36c8205f86a
Nextor <nestor.panu@gmail.com>
2021-02-28 22:39:54 -0500

    Improving Expenses Summary

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -5,16 +5,23 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     return (
-        <p>
-            {`Viewing ${props.expenses.length} ${(props.expenses.length != 1 ? ' expenses' : ' expense' )} totalling ${numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}`}
-        </p>
+        <div>
+            <p>
+                {`Viewing ${expenseCount} ${(expenseCount != 1 ? ' expenses' : ' expense' )} totalling ${numeral(expensesTotal / 100).format('$0,0.00')}`}
+            </p>
+        </div>
     );
 };
 
-const mapStateToProps = (state) => ({
-    expenses: selectExpenses(state.expenses, state.filters)
-});
+const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    };
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);
